828c57afb66764aaed9e9f7939cdc7556ffb9cf3
dhuth <derickhuth@gmail.com>
2014-08-04 18:51:18 -0600

    --

--- hohmann.py
+++ hohmann.py
@@ -1,179 +1,43 @@
 from __future__ import print_function, division
 
 import collections
-TransferParams = collections.namedtuple('TransferParams', ['tof','cosdt','sindt','r1','r2','k','l','m','u'])
-del collections
+import scipy.optimize
 
-class TransferException(Exception):
-    def __init__(self, msg):
-        Exception.__init__(self, msg)
+from numpy import sqrt
 
-def getparameters(r1vec, r2vec, tof, u):
-    '''Create a set of transfer parameters.'''
-    from ksptools.util import veccos, vecsin, unit
-    from numpy import sqrt
+from .orbit import spec_energy, time_from_pe, semi_major_axis, eccentricity
+from .util import arcvec
+
+TransferParamters = collections.namedtuple("TransferParamters", ['u','r1','v1','r2','v2','t0','t1', 'dta'])
+
+def commonplane(kep1, kep2, t0, t1):
+    r1, v1 = kep1.rv(t0)
+    r2, v2 = kep2.rv(t1)
+    r2 = kep1.orientation / (kep2.orientation * r2)
+    v2 = kep1.orientation / (kep2.orientation * v2)
+    return r1, v1, r2, v2, lon_asc
+
+def solve_transfer(u, kep1, kep2, t0, t1, method='ballistic'):
+    r1, v1 = kep1.rv(t0)
+    r2, v2 = kep2.rv(t1)
+    
+    if method == 'ballistic':
+        dta = arcvec(r1, r2)
+        return slove_transfer_planar(TransferParameters(u, r1, v1, r2, v2, t0, t1, dta))
+
+def solve_transfer_planar(params, pick, toffunc, compute):
+    x = pick(params)
+    scipy.optimize.minimize(lambda i: abs(toffunc(i, params) - (t1 - t0)), [x])
+    return compute(x)
+
+def pickv(params):
     from numpy.linalg import norm
-    from collections import namedtuple
-    
-    cosdt = veccos(r1vec, r2vec)
-    sindt = vecsin(r1vec, r2vec)
-    
-    r1, r2 = norm(r1vec), norm(r2vec)
-    k = r1*r2*(1.-cosdt)
-    l = r1 + r2
-    m = r1*r2*(1.+cosdt)
-    
-    return TransferParams(tof, cosdt, sindt, r1, r2, k, l, m, u)
+    return params.v1
 
-def getfg(p, params):
-    '''Calculate f, g, f prime, and g prime.'''
-    assert(p > 0)
-    assert(p < float('inf'))
-    from numpy import sqrt, tan, arccos
-    
-    tof, cosdt, sindt, r1, r2, k, l, m, u = params
-    tanhalfdt = tan(arccos(cosdt)/2.)
-    f = 1. - (r2/p)*(1.-cosdt)
-    g = r1*r2*sindt/sqrt(u*p)
-    fp = sqrt(u/p)*tanhalfdt*((1.-cosdt)/p - 1./r1 - 1./r2)
-    gp = 1. - (r1/p)*(1.-cosdt)
-    return f, g, fp, gp
-
-def geta(p, m, k, l):
-    '''Calculate the semi-major axis'''
-    assert(p > 0)
-    return m*k*p/((2.*m-l**2)*p**2. + 2.*k*l*p - k**2)
-
-def getdE(a, f, fp, r1, r2, u):
-    '''Calculate the change in eccentric anomally for an elliptical orbit.'''
-    assert(a > 0)
-    from numpy import sqrt, arctan, arctan2, pi, sin, cos, arccos, arcsin
-    cosdE = 1. - (r1/a)*(1.-f)
-    sindE = -(r1*r2*fp)/sqrt(u*a)
-    _a = min(1.0, max(-1.0, cosdE))
-    dE = arccos(_a)
-    if sindE < 0:
-        dE = 2*pi - dE
-    assert(dE > 0)
-    return dE, cosdE, sindE
-
-def getdF(a, f, r1):
-    '''Calculate the change in eccentric anomally for a hyperbolic orbit.'''
-    assert(a < 0)
-    from numpy import arccosh, sinh
-    coshdF = 1. - (r1/a)*(1.-f)
-    dF = arccosh(coshdF)
-    sinhdF = sinh(dF)
-    return dF, coshdF, sinhdF
-
-def getv1v2(p, r1, r2, params):
-    '''Calculate orbital velocities at r1 and r2'''
-    f, g, fp, gp = getfg(p, params)
-    v1 = (r2-f*r1)/g
-    v2 = fp*r1 + gp*v1
-    return v1, v2
-
-def toffunc(p, *params):
-    '''Calculate time of flight by semi-latus rectum'''
-    tof, _, _ = gaussfunc(p, TransferParams(*params))
-    return tof
-
-def toferrfunc(p, *params):
-    '''Calculate difference of target time of flight and orbital time of flight.'''
-    params = TransferParams(*params)
-    etof = params.tof
-    vtof, _ = gaussfunc(p, params)
-    return etof - vtof
-
-def toferr2func(p, *params):
-    '''Calculate the square difference of target time of flight and orbital time of flight.'''
-    params = TransferParams(*params)
-    etof = params.tof
-    vtof, _ = gaussfunc(p, params)
-    return (etof - vtof)**2
-
-def gaussfunc(p, params):
-    '''Compute orbital parameters for a semi-latus rectum.'''
-    from numpy import sqrt
-    tof, cosdt, sindt, r1, r2, k, l, m, u = params
-    f, g, fp, gp = getfg(p, params)
-    a = geta(p, m, k, l)
-    #e = sqrt(1-p/a)
-    if a > 0:
-        dE, cosdE, sindE = getdE(a, f, fp, r1, r2, u)
-        tof = g + sqrt(a**3/u)*(dE-sindE)
-    elif a < 0:
-        dF, coshdF, sinhdF = getdF(a, f, r1)
-        tof = g + sqrt((-a)**3/u)*(sinhdF-dF)
-    return (tof, a)
-
-def planar_transfer(r1vec, r2vec, tof, u, err=1e-5, maxp=1.0e+21):
-    '''Calculate a transfer on a slingle plane.'''
-    from numpy import array, sqrt, arcsin, empty, finfo, seterr
-    from numpy.polynomial.polynomial import polyroots
-    from scipy.optimize import minimize, minimize_scalar, fminbound, bisect, brentq, brenth
-    
-    ## get constant parameters ##
-    params = getparameters(r1vec, r2vec, tof, u)
-    tof, cosdt, sindt, r1, r2, k, l, m, u = params
-    
-    ## pick bounds for the semi-latus rectum ##
-    p_i  = k/(l+sqrt(2*m))
-    p_ii = k/(l-sqrt(2*m))
-    if sindt > 0.0:
-        # dt < pi
-        pmin = p_i
-        pmax = None
-    elif sindt < 0.0:
-        # dt > pi
-        pmin = 0.
-        pmax = p_ii
-    elif sindt == 0.0:
-        raise TransferException("True anommoly is zero or pi.")
-    
-    ## find points where orbit switches between hyperbolic and elliptic ##
-    ## define bounds accordingly                                        ##
-    bounds = [pmin] + [i for i in polyroots([-k**2, 2*k*l, 2*m-l**2]) if i >= pmin and i <= pmax] + [pmax]
-    
-    ## minimize over each boundry ##
-    minp = []
-    for i in range(len(bounds)-1):
-        iter_err = err
-        mn = bounds[i]
-        mx = bounds[i+1]
-        
-        # check for small intervals #
-        if mx is not None and abs(mx-mn) <= iter_err*4:
-            continue
-        
-        # fix mn and mx for algorithm #
-        if mx is None:
-            mx = maxp
-            p0 = min(params.r1, params.r2, maxp-iter_err)
-        else:
-            p0 = (mx+mn)/2
-        mx -= iter_err
-        mn += iter_err
-        
-        # check for intervals that do not intersect zero #
-        mxtof, _ = gaussfunc(mx, params)
-        mntof, _ = gaussfunc(mn, params)
-        if (mntof - tof)*(mxtof - tof) >= 0:
-            continue
-        
-        # bisect it down #
-        p0 = brenth(toferrfunc, mn, mx, args=tuple(params), maxiter=7, disp=False)
-        
-        default_err = seterr(divide='raise', invalid='raise')
-        optval = minimize(toferr2func, [p0], args=tuple(params), bounds=[(mn, mx)], method='TNC')
-        seterr(**default_err)
-        minp.append(optval)
-    
-    if not len(minp):
-        return None
-    
-    ## find the very best solution ##
-    p = min(minp, key=lambda opt: opt.fun).x[0]
-    return getv1v2(p, r1vec, r2vec, params)
-
-    
+def tofv(v0, params):
+    u, r1 = params.u, params.r1
+    a = semi_major_axis(u=u, r=r1, v=v0)
+    e = eccentricity(u=u, a=a, r=r1, v=v0)
+    t1 = time_from_pe(u=u, a=a, e=e, r=r1)
+    t2 = time_from_pe(u=u, a=a, e=e, r=r2)
+    return t2 - t1
--- ksptools/orbit.py
+++ ksptools/orbit.py
@@ -1,9 +1,155 @@
 from __future__ import division
 
+from numpy import array, dot, cross, sqrt, pi
+from numpy import cos, sin, tan, arcsin, arccos, arctan
+from numpy import cosh, sinh, tanh, arccosh, arcsinh, arctanh
+from numpy.linalg import norm
+from scipy.optimize import newton
+from .util import EulerAngle, veccos, vecsin, cossin, Ax, rotz
+
+zeropos = array([1.,0.,0.,])
+
+def isnotNone(*args):
+    return all(x is not None for x in args)
+
+def spec_energy(u,r=None,a=None,e=None,v=None):
+    # 1. u, v, r
+    if isnotNone(u, v, r):
+        return dot(v,v)/2 - u/norm(r)
+    
+    # 2. u, h, e
+    if isnotNone(u, h, e):
+        return -(u**2/(2*h**2))*(1-e**2)
+    
+    # 3. a, u
+    if isnotNone(u, a):
+        return -u/2*a
+    
+    return NotImplementedError
+
+def spec_angular_momentum(r, v):
+    return norm(cross(r, v))
+
+def semi_major_axis(u,r=None,v=None,se=None):
+    if isnotNone(u,r,v):
+        return norm(r)*u/(2*u-norm(r)*dot(v,v))
+    if isnotNone(u,sec):
+        return -u/(2*sec)
+        
+    return NotImplementedError
+
+def eccentricity(u, se=None, sam=None, r=None, v=None, a=None):
+    # a. a, u
+    if isnotNone(a,u):
+        se = -u/2*a
+    
+    # 1. se, sam
+    if isnotNone(se, sam):
+        return sqrt(1+(2*se*sam**2)/u**2)
+    
+    # 2. se, r, v (r and v must be vectors)
+    if isnotNone(se, r, v):
+        return sqrt(1+(2*norm(cross(r, v))**2)/u**2)
+    
+    return NotImplementedError
+
+def true_anomaly(a=None,e=None,r=None,E=None,F=None,dt=None,mm=None):
+    # 1. a, e, r
+    if isnotNone(a,e,r):
+        if e != 1. and e > 0.0:
+            ta = arccos((a*(1-e**2)-norm(r))/(norm(r)*e))
+        elif e == 0.
+            ta = veccos(r,array([1.,0.,0.]))
+        if e < 1. and ta < 0.:
+            ta = 2*pi - ta
+        return ta
+    
+    # 2. e, E
+    if isnotNone(e,E):
+        return 2*arctan(sqrt((1.+e)/(1.-e))*tan(E/2.))
+    
+    # 3. e, F
+    if isnotNone(e,F):
+        ta = arccos((e-cosh(F))/(e*cosh(F)-1))
+        if F < 0:
+            return -ta
+        else:
+            return ta
+    
+    return NotImplementedError
+
+def eccentric_anomaly(e, ta=None, dt=None, M0=None, mm=None, a=None, u=None, r=None):
+    # a. a, u
+    if isnotNone(a,u):
+        if a > 0:
+            mm = sqrt(a**3/u)
+        elif a < 0:
+            mm = sqrt((-a)**3/u)
+        else:
+            raise NotImplementedError
+    
+    # b. a, e, r
+    if isnotNone(a,e,r):
+        ta = true_anomaly(a, e, r)
+    
+    # 2. e, ta
+    if isnotNone(e,ta):
+        if e == 0:
+            return ta
+        elif e < 1:
+            E = arccos((e+cos(ta))/(1+e*cos(ta)))
+            if E < 0:
+                E = 2*pi - E
+            return E
+        elif e > 1:
+            F = arccosh((e+cos(ta))/(1+e*cos(ta)))
+            return F
+    
+    # 3. e, dt, M0, mm
+    if isnotNone(e,dt,M0,mm):
+        M = M0 + mm*dt
+        if e < 1:
+            f = lambda nE: nE - e*sin(nE) - M
+            fp = lambda nE: 1 - e*cos(nE)
+            fp2 = lambda nE: e*sin(nE)
+        elif e > 1:
+            f = lambda nF: e*sinh(nF) - nF - M
+            fp = lambda nF: e*cosh(nF) - 1
+            fp2 = lambda nF: e*sinh(nF)
+        else:
+            raise NotImplementedError
+        return newton(f,0,fp,fprime2=fp2)
+
+    return NotImplementedError
+
+def time_from_pe(u=None, a=None, e=None, EF=None, mm=None, ta=None, r=None):
+    # a. a, e, r
+    if isnotNone(a,e,r):
+        ta = true_anomaly(a=a,r=r,e=e)
+    
+    # b. u, a
+    if isnotNone(u,a):
+        if a > 0:
+            mm = sqrt(a**3/u)
+        elif a < 0:
+            mm = sqrt((-a)**3/u)
+    
+    # b. ta, e
+    if isnotNone(ta, e):
+        EF = eccentric_anomally(e=e, ta=ta)
+    
+    # 1. EF, e, mm
+    if isnotNone(EF,e,m):
+        if e < 1:
+            return (EF - e*sin(EF))*mm
+        elif e > 1:
+            return (e*sinh(EF) - EF)*mm
+    
+    return NotImplementedError
+    
+
 class KeplerOrbit(object):
     def __init__(self, u, a, e, inc, lon_asc, arg_pe, M, epoch):
-        from numpy import sqrt, pi
-        from .util import EulerAngle
         self.u = u
         self.e = e
         self.p = a*(1-e**2)
@@ -23,9 +169,6 @@ class KeplerOrbit(object):
     
     @classmethod
     def from_rvu(cls, rvec, velvec, u, epoch=0.):
-        from numpy import cross, array, dot, arccos, arccosh, sin, sinh, cos, pi
-        from numpy.linalg import norm
-        from .util import veccos
         r = norm(rvec)
         v = norm(velvec)
         evec = (1./u)*((v**2-u/r)*rvec-dot(rvec,velvec)*velvec)
@@ -63,8 +206,6 @@ class KeplerOrbit(object):
         return cls(u, a, e, i, la, argpe, M, epoch)
 
     def E_anom(self, dt):
-        from numpy import cos, sin
-        from scipy.optimize import newton
         M = self.mean_anom + (self.mean_motion * dt)
         e = self.e
         f = lambda nE: nE - e*sin(nE) - M
@@ -73,8 +214,6 @@ class KeplerOrbit(object):
         return newton(f,0,fp,fprime2=fp2)
     
     def F_anom(self, dt):
-        from numpy import cosh, sinh
-        from scipy.optimize import newton
         M = self.mean_anom + (self.mean_motion * dt)
         e = self.e
         f = lambda nF: e*sinh(nF) - nF - M
@@ -83,7 +222,6 @@ class KeplerOrbit(object):
         return newton(f,0,fp,fprime2=fp2)
     
     def true_anom(self, t):
-        from numpy import arctan, arccos, tan, sqrt, cosh
         t -= self.epoch
         e = self.e
         a = self.a
@@ -100,18 +238,13 @@ class KeplerOrbit(object):
         raise Exception("Peribolic orbits are not supported")
     
     def flightvec(self, t, theta=None):
-        from .util import cossin
-        from numpy import array, arctan, arctan2, pi
         e = self.e
         if theta is None:
             theta = self.true_anom(t)
         ct, st, _ = cossin(theta)
         return array([-st, e+ct, 0])
-        #return 0.
     
     def prograde(self, t, theta=None):
-        from .util import cossin, Ax, rotz
-        from numpy import array
         e = self.e
         if theta is None:
             theta = self.true_anom(t)
@@ -121,8 +254,6 @@ class KeplerOrbit(object):
         return self.orient.rotate(pgd)
     
     def radialin(self, t, theta=None):
-        from .util import cossin, Ax, rotz
-        from numpy import array
         e = self.e
         if theta is None:
             theta = self.true_anom(t)
@@ -131,12 +262,9 @@ class KeplerOrbit(object):
         return self.orient.rotate(rad)
     
     def normal(self):
-        from numpy import array
         return self.orient.rotate(array([0.,0.,1.]))
     
     def r(self, t, theta=None):
-        from .util import cossin
-        from numpy import cos
         p,e = self.p, self.e
         if theta is None:
             theta = self.true_anom(t)
@@ -144,14 +272,11 @@ class KeplerOrbit(object):
         return self.orient.rotate(cossin(theta))*l
     
     def pe(self):
-        from numpy import array
         p = self.p
         e = self.e
         return self.orient.rotate(array([1.,0.,0.]))*p/(1+e)
     
     def v(self, t, theta=None):
-        from .util import cossin, Ax, rotz
-        from numpy import array, arctan2, sqrt
         p,e,a,u = self.p, self.e, self.a, self.u
         if theta is None:
             theta = self.true_anom(t)
@@ -166,7 +291,6 @@ class KeplerOrbit(object):
         return self.r(t, theta), self.v(t, theta)
     
     def period(self):
-        from numpy import pi
         if self.a > 0:
             return 2*pi/self.mean_motion
         elif self.a < 0:
