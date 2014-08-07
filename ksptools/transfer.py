39901df8e155391f152576ef08963f9f10c4ec2a
dhuth <derickhuth@gmail.com>
2014-08-07 11:24:37 -0600

    Saving place.

--- ksptools/transfer.py
+++ ksptools/transfer.py
@@ -3,7 +3,7 @@ from __future__ import print_function, division
 import collections
 import scipy.optimize
 
-from numpy import sqrt, dot, cos, sin, arccos, arcsin, pi, arccosh, sinh, tan, spacing, arctan, arctan2
+from numpy import sqrt, dot, cos, sin, arccos, arcsin, pi, arccosh, sinh, tan, spacing, arctan, arctan2, finfo
 from numpy.linalg import norm
 from .util import arcvec, cossin
 
@@ -16,13 +16,16 @@ def commonplane(kep1, kep2, t0, t1):
     v2 = kep1.orientation / (kep2.orientation * v2)
     return r1, v1, r2, v2, lon_asc
 
-def solve_transfer(u, kep1, kep2, t0, t1, method='ballistic'):
+def solve_transfer(u, kep1, kep2, t0, t1, method='ballistic', solver=SemiLatisSolver, output='kepler'):
     r1, v1 = kep1.rv(t0)
     r2, v2 = kep2.rv(t1)
     
     if method == 'ballistic':
         dta = arcvec(r1, r2)
-        return solve_transfer_planar(TransferParameters(u, r1, v1, r2, v2, t0, t1, dta), SemiLatisSolver())
+        mnsol, err, solv1, solv2 = solve_transfer_planar(TransferParameters(u, r1, v1, r2, v2, t0, t1, dta), solver())
+    
+    if output == 'debug':
+        return mnsol, err, solv1, solv2
 
 def solve_transfer_planar(params, solver):
     x = solver.start(params)
@@ -31,9 +34,10 @@ def solve_transfer_planar(params, solver):
     f = lambda i: (solver.tof(i, params) - (t1 - t0))**2
     fp = lambda i: 2*(solver.tof(i, params) - (t1 - t0))*solver.tofprime(i, params)
     
-    p = scipy.optimize.minimize(f, x, jac=fp, method='TNC')
-    print(p)
-    return solver.solution(p.x[0], params)
+    p = scipy.optimize.minimize(f, x, jac=fp, bounds=[(solver.minvalue, solver.maxvalue)], method='TNC')
+    err = sqrt(p.func)
+    solv1, solv2 = solver.solution(p.x[0], params)
+    return p, err, solv1, solv2
 
 
 class TransferSolver(object):
@@ -41,6 +45,25 @@ class TransferSolver(object):
 
 
 class SemiLatisSolver(TransferSolver):
+    def checkp(self, p, params):
+        if p <= self.minvalue:
+            print("bad p, {} < {}".format(p, self.minvalue))
+        elif p >= self.maxvalue:
+            print("bad p, {} > {}".format(p, self.maxvalue))
+        #else:
+        #    print("good p")
+        
+        #assert(p > self.minvalue)
+        #assert(p < self.maxvalue)
+        return p
+    
+    def checka(self, a, p, params):
+        if abs(a) == float('inf'):
+            print(a)
+            return (finfo(a).max)**(1/3.)
+        assert(a != 0)
+        return a
+    
     def start(self, params):
         cosdt, sindt, _ = cossin(params.dta)
         self.cosdt = cosdt
@@ -56,20 +79,19 @@ class SemiLatisSolver(TransferSolver):
         p_ii = self.k/(self.l - sqrt(2*self.m))
         
         if self.theta < pi:
-            self.minp = p_i
-            self.maxp = float('inf')
+            self.minvalue = p_i
+            self.maxvalue = float('inf')
             return p_i*3
         elif self.theta > pi:
-            self.minp = 0
-            self.maxp = p_ii
+            self.minvalue = 0
+            self.maxvalue = p_ii
             return p_ii/2
         else:
             raise Exception
     
     def tof(self, p, params):
-        assert(p >= self.minp)
-        assert(p <= self.maxp)
-        p = norm(p)
+        p = self.checkp(norm(p), params)
+        
         r1, r2 = norm(params.r1), norm(params.r2)
         cosdt, sindt = self.cosdt, self.sindt
         
@@ -81,14 +103,17 @@ class SemiLatisSolver(TransferSolver):
         m, k, l = self.m, self.k, self.l
         a = m*k*p/((2*m-l**2)*p**2 + 2*k*l*p - k**2)
         
+        a = self.checka(a,p,params)
+        
         if a > 0:
             cosE = 1 - r1*(1-f)/a
             sinE = -r1*r2*fp/sqrt(params.u*a)
-            E = arctan(sinE/cosE)
-            #E = arccos(cosE)
-            #if E < 0:
-            #   E = 2*pi + E
-            #assert(E >= 0)
+            #E = arctan(sinE/cosE)
+            #E = arcsin(cosE)
+            E = arccos(cosE)
+            if sinE < 0:
+               E = 2*pi - E
+            assert(E >= 0)
             return g + sqrt(a**3/params.u)*(E-sinE)
         elif a < 0:
             coshF = 1 - r1*(1-f)/a
@@ -116,5 +141,5 @@ class SemiLatisSolver(TransferSolver):
     def tofprime(self, p, params):
         #n = 3
         #eps = spacing(n*p)
-        return scipy.optimize.approx_fprime(p, lambda i: self.tof(i, params), 1e-3)
+        return scipy.optimize.approx_fprime(p, lambda i: self.tof(i, params), 1e-4)
         
