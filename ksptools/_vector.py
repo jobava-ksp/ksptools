e0d6d294ea8e6683f3e52617614bbf2484ab47ad
dhuth <derickhuth@gmail.com>
2014-09-22 15:13:21 -0600

    --

--- ksptools/_vector.py
+++ ksptools/_vector.py
@@ -2,6 +2,7 @@ from __future__ import division
 
 from numpy import array, cos, arccos
 from numpy.linalg import norm
+from scipy.integrate import odeint, ode
 
 
 class RVVector(object):
@@ -58,4 +59,14 @@ def state_vector(r,v):
 
 def perifocal_vector(r,v):
     return PerifocalVector(r,v)
+
+def compute_ode_time(stv, t0, t1, accel_func):
+    rv = array(list(stv.r) + list(stv.v))
+    print rv
+    def func(y, t):
+        return array(list(y[3:6]) + list(accel_func(y[0:3], y[3:6], t)))
+    
+    rv1 = odeint(func, rv, [t0, t1])[1]
+    return type(stv)(rv1[0:3], rv1[3:6])
+
 
